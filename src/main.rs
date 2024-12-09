7db6672efbf93270954fe2eb23cd066aef677de7
whattablackhole <rudolf.gritsenyak@gmail.com>
2024-12-09 13:06:49 +0300

    feat: setup freestanding binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 #![no_std]
-fn main() {
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[cfg(not(test))]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
